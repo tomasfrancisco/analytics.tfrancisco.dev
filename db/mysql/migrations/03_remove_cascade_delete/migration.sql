75f5e02ca13224f98c2d92c2ceb8dde4ea1de02d
Tomas Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2022-11-09 14:17:47 +0100

    Revert "chore: remove drop of fks"
    This reverts commit 939996e56c8337622697f48278142423abaadb77.

--- db/mysql/migrations/03_remove_cascade_delete/migration.sql
+++ db/mysql/migrations/03_remove_cascade_delete/migration.sql
@@ -1,2 +1,18 @@
+-- DropForeignKey
+ALTER TABLE `event` DROP FOREIGN KEY `event_ibfk_2`;
 
+-- DropForeignKey
+ALTER TABLE `event` DROP FOREIGN KEY `event_ibfk_1`;
+
+-- DropForeignKey
+ALTER TABLE `pageview` DROP FOREIGN KEY `pageview_ibfk_2`;
+
+-- DropForeignKey
+ALTER TABLE `pageview` DROP FOREIGN KEY `pageview_ibfk_1`;
+
+-- DropForeignKey
+ALTER TABLE `session` DROP FOREIGN KEY `session_ibfk_1`;
+
+-- DropForeignKey
+ALTER TABLE `website` DROP FOREIGN KEY `website_ibfk_1`;
 
